28b6719ffda79d8043834aa98a5cb7d40d7afd62
Roman Pigunov <jerome.malone@yandex.ru>
2026-02-14 17:48:43 +0300

    1

---
+++ scripts/leagues.js
@@ -0,0 +1,39 @@
+const leagues = {
+  ucl: {
+    name: "Champions League",
+    background: "linear-gradient(93.5deg,#0232ff 0.59%,#010056 94.82%)",
+    button: "#040a63",
+    players: {
+      Камиль: 664,
+      Хидир: 579,
+      Эрик: 502,
+      Карузо: 631,
+      Аскольд: 435,
+    },
+  },
+  pl: {
+    name: "Premier League",
+    background: "linear-gradient(90deg,#37003c,#953bff)",
+    button: "#37003c",
+    players: {
+      Камиль: 1170,
+    },
+  },
+  laLiga: {
+    name: "LaLiga",
+    background: "#ff4b44",
+    button: "#ff4b44",
+    players: {
+      Камиль: 883,
+    },
+  },
+  seriaA: {
+    name: "Seria A",
+    background:
+      "url(https://shoneal.github.io/fantasy/images/other/seriA_cover.webp) no-repeat center center / cover",
+    button: "#2047e3",
+    players: {
+      Камиль: 1128,
+    },
+  },
+}; // Данные лиг
