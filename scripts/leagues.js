21a2fd0df3e1dedda1be0a44e59724d494e81dbf
Roman Pigunov <jerome.malone@yandex.ru>
2026-02-24 11:24:27 +0300

    1

--- scripts/leagues.js
+++ scripts/leagues.js
@@ -4,11 +4,11 @@ export const leagues = {
     background: "linear-gradient(93.5deg,#0232ff 0.59%,#010056 94.82%)",
     button: "#040a63",
     players: {
-      Камиль: 664,
-      Хидир: 579,
-      Эрик: 502,
-      Карузо: 631,
-      Аскольд: 435,
+      Камиль: 714,
+      Хидир: 636,
+      Эрик: 545,
+      Карузо: 690,
+      Аскольд: 463,
     },
   },
   pl: {
@@ -16,7 +16,7 @@ export const leagues = {
     background: "linear-gradient(90deg,#37003c,#953bff)",
     button: "#37003c",
     players: {
-      Камиль: 1170,
+      Камиль: 1219,
     },
   },
   laLiga: {
@@ -24,7 +24,7 @@ export const leagues = {
     background: "#ff4b44",
     button: "#ff4b44",
     players: {
-      Камиль: 883,
+      Камиль: 974,
     },
   },
   seriaA: {
@@ -33,7 +33,7 @@ export const leagues = {
       "url(https://shoneal.github.io/fantasy/images/other/seriA_cover.webp) no-repeat center center / cover",
     button: "#2047e3",
     players: {
-      Камиль: 1128,
+      Камиль: 1215,
     },
   },
 }; // Данные лиг
